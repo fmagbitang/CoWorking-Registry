5c19de7d881ec769c6c702d6f3cae5abb91b1c85
Joanna <j.apellido601@mybvc.ca>
2023-08-12 14:58:56 -0600

    Added Home page and logout

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -8,4 +8,18 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+//Route for the create properties fot the owner
+router.get('/owner/properties/', (req,res) => {
+  res.sendFile(path.join(__dirname, '../public/propeties.html'));
+});
+
+//Route for the create workspace fot the owner
+router.get('/owner/workspace/', (req,res) => {
+  res.sendFile(path.join(__dirname, '../public/workspace.html'));
+});
+
+router.get('/homePage', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/homePage.html'))
+});
+
 module.exports = router;
